e5446d3f1757c1bae8bcb36cf7420f323cd76f67
Kumaru2 <60379752+Kumaru2@users.noreply.github.com>
2020-12-09 11:32:11 +0100

    Update validate.js
    Co-Authored-By: Tmptvk <54776913+Tmptvk@users.noreply.github.com>

--- javascript/validate.js
+++ javascript/validate.js
@@ -21,7 +21,8 @@ function validateForm2() {
     cbox[0].checked == false &&
     cbox[1].checked == false &&
     cbox[2].checked == false &&
-    cbox[3].checked == false
+    cbox[3].checked == false &&
+    cbox[4].checked == false
   ) {
 
     // skal vores validerings div vise sig
@@ -148,5 +149,3 @@ function validateForm4() {
       }
 
       spilnu2.addEventListener('click', finalChange2);
-
-      
